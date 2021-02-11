fb81ef037a59d48bfbfe8f20ab4cf8f555742196
Reid Metzger <contact@reidmetzger.com>
2021-02-11 18:25:07 -0500

    Compare price of building to current cookies

--- cookiebot.js
+++ cookiebot.js
@@ -1,2 +1,6 @@
 console.log(`Cookies: ${Game.Cookies}`)
 console.log(`Price: ${Game.ObjectsById['2'].bulkPrice}`)
+
+if (Game.cookies >= Game.ObjectsById['2'].bulkPrice){
+    console.log("Buy Now")
+}
