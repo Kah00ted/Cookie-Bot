729e1ce735515f908d104baf2c584699fbebfc74
Reid Metzger <contact@reidmetzger.com>
2021-02-12 12:42:53 -0500

    Add loop

--- cookiebot.js
+++ cookiebot.js
@@ -11,32 +11,34 @@ cookieGoals = {
     "temple":0,
     "wizzard":0,
     "shipment":0,
-    "alchLab":2,
-    "portal":0
+    "alchLab":3,
+    "portal":10
 }
 
-if (Game.ObjectsById['0'].amount < cookieGoals.cursor){
-    tryBuy(0)
-}else if (Game.ObjectsById['1'].amount < cookieGoals.grandma){
-    tryBuy(1)
-}else if (Game.ObjectsById['2'].amount < cookieGoals.farm){
-    tryBuy(2)
-}else if (Game.ObjectsById['3'].amount < cookieGoals.mine){
-    tryBuy(3)
-}else if (Game.ObjectsById['4'].amount < cookieGoals.factory){
-    tryBuy(4)
-}else if (Game.ObjectsById['5'].amount < cookieGoals.bank){
-    tryBuy(5)
-}else if (Game.ObjectsById['6'].amount < cookieGoals.temple){
-    tryBuy(6)
-}else if (Game.ObjectsById['7'].amount < cookieGoals.wizzard){
-    tryBuy(7)
-}else if (Game.ObjectsById['8'].amount < cookieGoals.shipment){
-    tryBuy(8)
-}else if (Game.ObjectsById['9'].amount < cookieGoals.alchLab){
-    tryBuy(9)
-}else if (Game.ObjectsById['10'].amount < cookieGoals.portal){
-    tryBuy(10)
+function scanCookies(){
+    if (Game.ObjectsById['0'].amount < cookieGoals.cursor){
+        tryBuy(0)
+    }else if (Game.ObjectsById['1'].amount < cookieGoals.grandma){
+        tryBuy(1)
+    }else if (Game.ObjectsById['2'].amount < cookieGoals.farm){
+        tryBuy(2)
+    }else if (Game.ObjectsById['3'].amount < cookieGoals.mine){
+        tryBuy(3)
+    }else if (Game.ObjectsById['4'].amount < cookieGoals.factory){
+        tryBuy(4)
+    }else if (Game.ObjectsById['5'].amount < cookieGoals.bank){
+        tryBuy(5)
+    }else if (Game.ObjectsById['6'].amount < cookieGoals.temple){
+        tryBuy(6)
+    }else if (Game.ObjectsById['7'].amount < cookieGoals.wizzard){
+        tryBuy(7)
+    }else if (Game.ObjectsById['8'].amount < cookieGoals.shipment){
+        tryBuy(8)
+    }else if (Game.ObjectsById['9'].amount < cookieGoals.alchLab){
+        tryBuy(9)
+    }else if (Game.ObjectsById['10'].amount < cookieGoals.portal){
+        tryBuy(10)
+    }
 }
 
 function tryBuy(buildingId){
@@ -47,3 +49,12 @@ function tryBuy(buildingId){
         console.log("Not Enough Cookies")
     }
 }
+
+function cookieBotLoop() {         
+    setTimeout(function() {
+        scanCookies()
+        cookieBotLoop()               
+    }, 10000)
+}
+
+cookieBotLoop()
